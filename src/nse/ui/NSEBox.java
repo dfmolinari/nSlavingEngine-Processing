1cdf9f0cc4546a8184bfd0fa4833b14eca637695
dfmolinari <66701511+dfmolinari@users.noreply.github.com>
2020-07-24 02:00:09 +0200

    README edits and added PVector implementation for buttons and boxes

--- src/nse/ui/NSEBox.java
+++ src/nse/ui/NSEBox.java
@@ -6,8 +6,8 @@ public class NSEBox {
 
     private PApplet myApplet;
 
-    private float x, y;
-    private float w, h;
+    private PVector m_position;
+    private PVector m_size;
     private BoxType type;
     private int baseColor, textColor;
     private String label;
@@ -15,11 +15,22 @@ public class NSEBox {
     private PFont font;
     private float fontSize;
 
+    public NSEBox(PVector position, PVector size, BoxType type, String label, int baseColor, int textColor, PImage img, float fontSize, PFont font, PApplet myApplet){
+        m_position = new PVector(position.x,position.y);
+        m_size = new PVector(size.x,size.y);
+        this.type = type;
+        this.label = label;
+        this.baseColor = baseColor;
+        this.textColor = textColor;
+        this.img = img;
+        this.fontSize = fontSize;
+        this.font = font;
+        this.myApplet = myApplet;
+    }
+
     public NSEBox(float x, float y, float w, float h, BoxType type, String label, int baseColor, int textColor, PImage img, float fontSize, PFont font, PApplet myApplet){
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
+        m_position = new PVector(x,y);
+        m_size = new PVector(w,h);
         this.type = type;
         this.label = label;
         this.baseColor = baseColor;
@@ -34,7 +45,7 @@ public class NSEBox {
         if(type == BoxType.Default){
             myApplet.rectMode(myApplet.CENTER);
             myApplet.fill(baseColor);
-            myApplet.rect(x,y,w,h);
+            myApplet.rect(m_position.x, m_position.y, m_size.x, m_size.y);
             myApplet.fill(textColor);
             myApplet.textAlign(myApplet.CENTER, myApplet.CENTER);
 
@@ -42,16 +53,16 @@ public class NSEBox {
                 myApplet.textFont(font);
 
             myApplet.textSize(fontSize);
-            myApplet.text(label,x,y,w,h);
+            myApplet.text(label,m_position.x, m_position.y, m_size.x, m_size.y);
             myApplet.fill(255);
             myApplet.rectMode(myApplet.CORNER);
         } else if(type == BoxType.Image){
             myApplet.imageMode(myApplet.CENTER);
-            myApplet.image(img,x,y,w,h);
+            myApplet.image(img,m_position.x, m_position.y, m_size.x, m_size.y);
             myApplet.imageMode(myApplet.CORNER);
         } else if(type == BoxType.Mixed){
             myApplet.imageMode(myApplet.CENTER);
-            myApplet.image(img,x,y,w,h);
+            myApplet.image(img,m_position.x, m_position.y, m_size.x, m_size.y);
             myApplet.imageMode(myApplet.CORNER);
             myApplet.fill(textColor);
             myApplet.textAlign(myApplet.CENTER,myApplet.CENTER);
@@ -61,7 +72,7 @@ public class NSEBox {
             
             myApplet.textSize(fontSize);
             myApplet.rectMode(myApplet.CENTER);
-            myApplet.text(label,x,y,w,h);
+            myApplet.text(label,m_position.x, m_position.y, m_size.x, m_size.y);
             myApplet.rectMode(myApplet.CORNER);
             myApplet.fill(255);
         }
--- src/nse/ui/NSEButton.java
+++ src/nse/ui/NSEButton.java
@@ -8,8 +8,8 @@ public class NSEButton {
 
     private InputManager im;
 
-    private float x, y;
-    private float w, h;
+    private PVector m_position;
+    private PVector m_size;
 
     private ButtonType type;
 
@@ -24,11 +24,25 @@ public class NSEButton {
     private boolean isEnabled;
     private boolean isActive;
 
+    public NSEButton(PVector position, PVector size, ButtonType type, String label, int baseColor, int textColor, PImage img, float fontSize, PFont font, PApplet main){
+        m_position = new PVector(position.x,position.y);
+        m_size = new PVector(size.x,size.y);
+        this.type = type;
+        this.label = label;
+        this.baseColor = baseColor;
+        this.textColor = textColor;
+        this.img = img;
+        this.fontSize = fontSize;
+        this.font = font;
+        this.myApplet = main;
+        im = new InputManager(myApplet,this);
+        isEnabled = true;
+        isActive = true;
+    }
+
     public NSEButton(float x, float y, float w, float h, ButtonType type, String label, int baseColor, int textColor, PImage img, float fontSize, PFont font, PApplet main){
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
+        m_position = new PVector(x,y);
+        m_size = new PVector(w,h);
         this.type = type;
         this.label = label;
         this.baseColor = baseColor;
@@ -47,7 +61,7 @@ public class NSEButton {
             if (type == ButtonType.Default) {
                 myApplet.rectMode(myApplet.CENTER);
                 myApplet.fill(baseColor);
-                myApplet.rect(x, y, w, h);
+                myApplet.rect(m_position.x, m_position.y, m_size.x, m_size.y);
                 myApplet.fill(textColor);
                 myApplet.textAlign(myApplet.CENTER, myApplet.CENTER);
 
@@ -55,16 +69,16 @@ public class NSEButton {
                     myApplet.textFont(font);
 
                 myApplet.textSize(fontSize);
-                myApplet.text(label, x, y, w, h);
+                myApplet.text(label, m_position.x, m_position.y, m_size.x, m_size.y);
                 myApplet.fill(255);
                 myApplet.rectMode(myApplet.CORNER);
             } else if (type == ButtonType.Image) {
                 myApplet.imageMode(myApplet.CENTER);
-                myApplet.image(img, x, y, w, h);
+                myApplet.image(img, m_position.x, m_position.y, m_size.x, m_size.y);
                 myApplet.imageMode(myApplet.CORNER);
             } else if (type == ButtonType.Mixed) {
                 myApplet.imageMode(myApplet.CENTER);
-                myApplet.image(img, x, y, w, h);
+                myApplet.image(img, m_position.x, m_position.y, m_size.x, m_size.y);
                 myApplet.imageMode(myApplet.CORNER);
                 myApplet.fill(textColor);
                 myApplet.textAlign(myApplet.CENTER, myApplet.CENTER);
@@ -74,7 +88,7 @@ public class NSEButton {
 
                 myApplet.textSize(fontSize);
                 myApplet.rectMode(myApplet.CENTER);
-                myApplet.text(label, x, y, w, h);
+                myApplet.text(label, m_position.x, m_position.y, m_size.x, m_size.y);
                 myApplet.rectMode(myApplet.CORNER);
                 myApplet.fill(255);
             }
@@ -106,8 +120,8 @@ public class NSEButton {
         }
     }
 
-    public boolean mouseOver(){
-        if(myApplet.mouseX >= x-w/2 && myApplet.mouseX <= x+w/2 && myApplet.mouseY >= y-h/2 && myApplet.mouseY <= y+h/2) return true;
+    private boolean mouseOver(){
+        if(myApplet.mouseX >= m_position.x-m_size.x/2 && myApplet.mouseX <= m_position.x+m_size.x/2 && myApplet.mouseY >= m_position.y-m_size.y/2 && myApplet.mouseY <= m_position.y+m_size.y/2) return true;
 
         return false;
     }
